60fdf31924ac8057737eccdc4112d29b3b5d5e87
gho9016 <65549208+gho9016@users.noreply.github.com>
2021-02-24 04:35:19 +0900

    Add files via upload

---
+++ discord.py
@@ -0,0 +1,9 @@
+import discord
+
+client = discord.Client()
+
+@client.event
+async def on_redey():
+    print("봇 시작")
+@client.event
+client.run("ODAwMzM2NDY4MDgwOTg0MDg0.YAQpaw.BjVaBrpHBw5xjoWoe-h9UuacYtE")
