2db90e4d847a70a15de34099fb1f503bb762b9b1
gho9016 <65549208+gho9016@users.noreply.github.com>
2021-02-24 05:01:37 +0900

    Update discord.py

--- discord.py
+++ discord.py
@@ -4,9 +4,14 @@ import os
 client = discord.Client()
 
 @client.event
-async def on_redey():
-    print("봇 시작")
-
+async def on_ready():
+    print("Dunchendiscordbot online")
+    game = discord.Game("던첸봇일하는중...")
+    await client.change_presence(status=discord.Status.online, activity=game)
+@client.event
+async def on_message(message):
+    if message.content.startswith("!ping"):
+        await message.channel.send("ping!")
     
 access_token = os.environ["BOT_TOKEN"]
 client.run(access_token)
